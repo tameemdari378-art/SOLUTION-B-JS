021138c024523b9e8fef349d0d636e6a6420851e
TAYYABA <tameemdari378@gmail.com>
2026-05-16 10:01:25 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,82 @@
+// Question No =1
+// let num1 = Number(prompt("Enter frist number:"));
+// let num2 = Number(prompt("Enter second number:"));
+// document.writeln("Sum:" + (num1 + num2) + "<br>");
+// document.writeln("substraction:" + (num1 - num2) + "<br>");
+// document.writeln("multiplication:" + (num1 * num2) + "<br>");
+// document.writeln("division:" + (num1 / num2) + "<br>");
+
+// Question No=2
+// let nunmber = number(prompt("Enter your number:"));
+// if (number % 2 === 0) {
+    // document.writeln(number + "is Even" + "<br>")
+// }
+// else {
+    // document.writeln(number + "is Odd" + "<br>")
+// }
+
+// Question No =3
+// let age = Number(prompt("Enter your age:"));
+// if (age >= 18) {
+    // document.writeln("Eligible for voting" + "<br>");
+// }
+// else {
+    // document.writeln("Not Eligible for voting" + "<br>");
+// }
+
+//  Question No=4
+// var arr = [1, 2, 3, 4, 5];
+// var total = 0;
+// for (var i = 0; i <= arr.lenght; i++) {
+    // total += arr[i];
+// }
+// document.writeln("Total Sum:" + tatal + "<br>");
+
+// Question No=5
+// let str = prompt("enter a string:");
+// let reverse =
+    // str.split("").reverse().join("");
+// if (str === reverse) {
+    // document.writeln("Palindrome" + "<br>");
+// }
+// else {
+    // document.writeln("Not Palindrome" + "<br>");
+// }
+
+// Question No=6
+// function Largest(a, b, c) {
+    // return Math.max(a, b, c);
+// }
+// document.writeln("Largest Number:" + Largest(10, 25, 30) + "<br>");
+
+// Question No=7
+// for (var i = 1; i <= 20; i++) {
+    // if (i % 2 === 0) {
+        // document.writeln(i + " ");
+    // }
+// }
+// document.writeln("<br>");
+
+// Question No=8
+// let factNum = number(prompt("Enter a number for factorial:"));
+// let factorial = 1;
+// for (let i = 1; i <= factNum; i++) {
+    // factorial *= i;
+// }
+// document.writeln("Factorial:" + factorial + "<br>");
+
+// Question No=9
+// let names = ["Ali", "Ahemd", "Sara", "Fatima", "Ayesha"];
+// for (let i = names.lenght - 1; i >= 0; i--) {
+    // document.writeln(names[i] + "<br>");
+// }
+// document.write1n("<br>");
+
+// Question No=10
+// let password = prompt("Enter password:");
+// if (password.lenght < 8) {
+    // document.writeln("Weak Password");
+// }
+// else {
+    // document.writeln("Strong Password");
+// }
